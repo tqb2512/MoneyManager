73bcdf2f7c574e61e5eea2b2fb7b4901f3bae250
grvhuy <kaitoatat@gmail.com>
2023-05-24 23:50:27 +0700

    Add budget

--- src/screens/Budget/components/Categories.tsx
+++ src/screens/Budget/components/Categories.tsx
@@ -1,19 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
-type ComponentProps = {
-    categoryName: String,
-    onSelect: 
+export type Props = {
+  categoryName: String;
+  onSelect: (text: String) => void;
 }
 
-const Categories: React.FC<ComponentProps> = ({ categoryName }) => {
+const Categories: React.FC<Props> = ({ categoryName, onSelect }) => {
   return (
-    <View>
-      <Text>Categories</Text>
-    </View>
-  )
-}
+      <View style={{alignContent: 'center', alignItems: 'center'}}>
+        <TouchableOpacity
+          onPress={() => onSelect(categoryName)}
+          style={{marginLeft: 16, marginRight: 16, marginTop: 16, width: 72}}>
+          <Image
+            style={{
+              height: 32,
+              width: 32,
+              padding: 4,
+              borderRadius: 9999,
+              alignSelf: 'center',
+            }}
+            source={{
+              uri: 'https://static-00.iconduck.com/assets.00/burger-icon-512x473-15nsuo1h.png',
+            }}></Image>
+          <Text style={{textAlign: 'center'}}>{categoryName}</Text>
+        </TouchableOpacity>
+      </View>
+  );
+};
 
 export default Categories
-
-const styles = StyleSheet.create({})
