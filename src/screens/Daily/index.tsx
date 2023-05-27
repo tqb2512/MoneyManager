25610904bbb58a5931ba6880de20f417862d92ae
Trịnh Quốc Bảo <21521867@gm.uit.edu.vn>
2023-05-27 14:09:10 +0700

    !

--- src/screens/Daily/index.tsx
+++ src/screens/Daily/index.tsx
@@ -2,13 +2,18 @@ import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React, { useEffect } from 'react';
 import DailyView from './components/DailyView';
 import DailyHeader from './components/DailyHeader';
+import { Transaction } from '../../models/transaction';
+import { getDBConnection, getTransactions } from '../../services/db-services';
 
 const Daily = () => {
+
+  const [transactionsList, setTransactionsList] = React.useState<Transaction[]>([]);
+  
+
   return (
     <SafeAreaView>
         {/* Income, expense, total, bar */}
       {/* Show list view chi tiêt schi tiêu ngày */}
-
       <DailyView date='22' month='03' year='2003' income='69' expense='35' />
     </SafeAreaView>
   );
