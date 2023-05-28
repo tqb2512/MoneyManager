2008ff79dac46adc1969537113aa9a2e829429e0
Trịnh Quốc Bảo <21521867@gm.uit.edu.vn>
2023-05-28 14:39:44 +0700

    !

--- src/screens/Home/Daily/index.tsx
+++ src/screens/Home/Daily/index.tsx
@@ -1,20 +1,39 @@
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React, { useEffect } from 'react';
-import DailyView from './components/DailyView';
-import DailyHeader from './components/DailyHeader';
+import DayInfo from './components/DayInfo';
 import { Transaction } from '../../../models/transaction';
-import { getDBConnection, getTransactions } from '../../../services/db-services';
+import { getDBConnection, getTransactionsByMonth, createTable, importTestData} from '../../../services/db-services';
 
 const Daily = () => {
 
   const [transactionsList, setTransactionsList] = React.useState<Transaction[]>([]);
+
+  useEffect(() => {
+    getDBConnection().then((db) => {
+      createTable(db);
+      //importTestData(db);
+    });
+
+    getDBConnection().then((db) => {
+      var date = new Date();
+      var month = date.getMonth() + 1;
+      var year = date.getFullYear();
+      getTransactionsByMonth(db, month, year).then((transactions) => {
+        setTransactionsList(transactions);
+      });
+    });
+  }, []);
+
   
 
   return (
     <SafeAreaView>
         {/* Income, expense, total, bar */}
       {/* Show list view chi tiêt schi tiêu ngày */}
-      <DailyView date='22' month='03' year='2003' income='69' expense='35' />
+      {transactionsList.map((transaction) => {
+        return <DayInfo {...transaction} />;
+      })
+      }
     </SafeAreaView>
   );
 };
