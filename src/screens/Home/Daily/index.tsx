b133dcadc3e3712e52824cf2e52ef98808ad2c72
Trịnh Quốc Bảo <21521867@gm.uit.edu.vn>
2023-05-29 18:17:41 +0700

    !

--- src/screens/Home/Daily/index.tsx
+++ src/screens/Home/Daily/index.tsx
@@ -10,6 +10,29 @@ const Daily = () => {
   const [DateList, setDateList] = React.useState<Date[]>([]);
 
   useEffect(() => {
+
+
+    getDBConnection().then((db) => {
+      /*dropDatabaseAndRecreate(db).then(() => {
+        console.log("Database dropped and recreated");
+      });*/
+
+      /*clearDatabase(db).then(() => {
+        console.log("Database cleared");
+      });*/
+      
+      /*createTable(db).then(() => {
+         console.log("Table created");
+      });*/
+
+      /*importTestData(db).then(() => {
+         console.log("Test data imported");
+      });*/
+    });
+
+
+
+
     var date = new Date();
     var month = date.getMonth() + 1;
     var year = date.getFullYear();
@@ -27,7 +50,7 @@ const Daily = () => {
 
       {DateList.map((date) => {
         return (
-          <DayBox date={date} />
+          <DayBox date={date}/>
         );
       })
       }
