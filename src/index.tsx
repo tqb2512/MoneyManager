870a0ea303de7900df3678fa64baea6c45da6e8c
Trịnh Quốc Bảo <21521867@gm.uit.edu.vn>
2023-06-01 22:43:25 +0700

    !

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import AppNavigation from "./navigation";
 import BottomBarTabs from "./navigation";
 import { NativeBaseProvider } from "native-base";
+import { Text } from "react-native";
 
 export default function App() {
     return (
